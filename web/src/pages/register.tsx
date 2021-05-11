03bfe5d9d9daadb2a57d05ece22b2b51465ee77d
sinansubasi95 <sinan95@gmail.com>
2021-05-12 01:46:03 +0300

    Register Error Handling

--- web/src/pages/register.tsx
+++ web/src/pages/register.tsx
@@ -4,18 +4,27 @@ import { Box, Button, FormControl, FormErrorMessage, FormLabel, Input } from '@c
 import { Wrapper } from '../components/Wrapper';
 import { InputField } from '../components/InputField';
 import { useRegisterMutation } from '../generated/graphql';
+import { toErrorMap } from '../utils/toErrorMap';
+import { useRouter } from 'next/router';
 
 interface registerProps {}
 
 const Register: React.FC<registerProps> = ({}) => {
+    const router = useRouter();
     const [,register] = useRegisterMutation();
     return (
         <Wrapper variant="small">
             <Formik
                 initialValues={{username: "", password: ""}}
-                onSubmit={async (values) => {
+                onSubmit={async (values, {setErrors}) => {
                     const response = await register(values); // spinner is spinning forever because we are not returning a promise
-                    response.data.register?.user?.id
+                    if(response.data?.register.errors) {
+                        // the reason why we don't need question mark here is typescript gonna infer that this is defined, because we have a statement in if condition
+                        setErrors(toErrorMap(response.data.register.errors)); 
+                    } else if (response.data?.register.user) {
+                        // worked
+                        router.push("/")
+                    }
                 }}
             >
                 {({ isSubmitting }) => (
